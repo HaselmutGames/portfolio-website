9a48ef171c486c2ba7314039a03281e1885c3f6a
HaselmutGames <haselmut.games@gmail.com>
2026-01-31 00:15:35 +0100

    fix routing issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+	import { resolve } from '$app/paths';
 </script>
 
 <header class="w-full bg-gray-900 shadow">
 	<nav class="mx-auto flex max-w-6xl items-center justify-between p-6">
 		<!-- Logo / Name -->
-		<a href="/" class="text-xl font-bold tracking-tight text-gray-400 hover:text-gray-200">
+		<a
+			href={resolve('/')}
+			class="text-xl font-bold tracking-tight text-gray-400 hover:text-gray-200"
+		>
 			HaselmutGames
 		</a>
 
@@ -14,10 +18,14 @@
 			class="flex gap-8 text-lg max-md:absolute max-md:top-full max-md:right-0 max-md:hidden
                max-md:flex-col max-md:bg-gray-900 max-md:p-4 max-md:shadow-md"
 		>
-			<li><a href="/projects" class="text-gray-400 hover:text-gray-200">Projects</a></li>
-			<li><a href="/experience" class="text-gray-400 hover:text-gray-200">Experience</a></li>
-			<li><a href="/stream" class="text-gray-400 hover:text-gray-200">Stream</a></li>
-			<li><a href="/contact" class="text-gray-400 hover:text-gray-200">Contact</a></li>
+			<li>
+				<a href={resolve('/projects/')} class="text-gray-400 hover:text-gray-200">Projects</a>
+			</li>
+			<li>
+				<a href={resolve('/experience')} class="text-gray-400 hover:text-gray-200">Experience</a>
+			</li>
+			<li><a href={resolve('/stream/')} class="text-gray-400 hover:text-gray-200">Stream</a></li>
+			<li><a href={resolve('/contact')} class="text-gray-400 hover:text-gray-200">Contact</a></li>
 		</ul>
 	</nav>
 </header>
